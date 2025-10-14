378104d10b0d6b09692b36521dde81c839af2ad5
Søren Højsgaard <sorenh@math.aau.dk>
2025-10-14 07:02:57 +0200

    SH: New paper added to citation file

--- inst/include/tntsupp.h
+++ inst/include/tntsupp.h
@@ -1,11 +1,9 @@
 #ifndef TNTSUPP_H
 #define TNTSUPP_H
 
+//using namespace std;
+
 #include <iostream>
-
-using namespace std;
-
-
 #include "tnt/subscript.h"
 #include "tnt/tnt.h"
 #include "tnt/vec.h"
@@ -259,7 +257,7 @@ inline Fortran_Matrix<T> operator*(const Region2D<Fortran_Matrix<T> > &A,
     Subscript M = A.num_rows();
     Subscript N = A.num_cols();
 
-    Subscript MN = M*N; 
+// unused    Subscript MN = M*N; 
 
     Fortran_Matrix<T> res(M,N);
     const T* a = A.begin();
